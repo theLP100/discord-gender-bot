51aaaf24becbdc9a898cdf79ad7a66b61077e3dd
LP <lp.passarelli.wilson@gmail.com>
2023-02-11 11:58:47 -0800

    bot now connects with discord. it prints hello world when >hi is given.  I can only get one command working at a time. (I had to comment out the hello-world part to get the ask-pronouns part to show up.  Next step: components are showing as strings.  figure out how to get buttons to show, and how to have multiple commands there.

--- genderbot.py
+++ genderbot.py
@@ -1,14 +1,39 @@
 import os
+
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
-load_dotenv
-TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client(intents = discord.Intents.default())
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+intents = discord.Intents(messages=True, guilds=True)
+intents.members = True
+intents.message_content = True
 
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
 
-client.run(TOKEN)
+bot = commands.Bot(command_prefix='>', intents = intents)
+
+# @bot.command(name='hi', help='prints hello world to greet you')
+# async def hello_world(ctx):
+#     response = "Hello World"
+#     await ctx.send(response)
+# bot.run(TOKEN)
+
+@bot.command(name='ask-pronouns', help='will ask user to click their pronouns and will add this to their nickname')
+async def ask_pronouns(ctx):
+    response = {"content": "This is a message with components:", "components": [
+        {
+            "type": 1,
+            "components": []
+        }
+    ]}
+    await ctx.send(response)
+bot.run(TOKEN)
+
+# client = discord.Client(intents = intents)
+# @client.event
+# async def on_ready():
+#     print(f'{client.user} has connected to Discord!')
+
+# client.run(TOKEN)
