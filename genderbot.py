b18aed52fedc8f1f297feb64a29811d2f691b1a3
LP <lp.passarelli.wilson@gmail.com>
2023-02-28 08:41:31 -0800

    fixed bot.command code so that both commands run.  going with bot.command, not client, since it is more robust.

--- genderbot.py
+++ genderbot.py
@@ -11,14 +11,12 @@ intents = discord.Intents(messages=True, guilds=True)
 intents.members = True
 intents.message_content = True
 
-
 bot = commands.Bot(command_prefix='>', intents = intents)
 
-# @bot.command(name='hi', help='prints hello world to greet you')
-# async def hello_world(ctx):
-#     response = "Hello World"
-#     await ctx.send(response)
-# bot.run(TOKEN)
+@bot.command(name='hi', help='prints hello world to greet you')
+async def hello_world(ctx):
+    response = "Hello World"
+    await ctx.send(response)
 
 @bot.command(name='ask-pronouns', help='will ask user to click their pronouns and will add this to their nickname')
 async def ask_pronouns(ctx):
@@ -31,9 +29,20 @@ async def ask_pronouns(ctx):
     await ctx.send(response)
 bot.run(TOKEN)
 
+
+
 # client = discord.Client(intents = intents)
+
 # @client.event
 # async def on_ready():
 #     print(f'{client.user} has connected to Discord!')
+
+# @client.event
+# async def on_message(message):
+#     if message.content.startswith('>hi'):
+#         await message.channel.send('Hello World!')
+
+#     elif message.content.startswith('>ask-pronouns'):
+#         await message.channel.send('Initiating pronoun asking sequence.')
 
 # client.run(TOKEN)
